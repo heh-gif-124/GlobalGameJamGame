d84e033556f917a256f498ce55ea1370017263e6
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-01-30 14:23:31 +0800

    more stuff

--- Src/Assets/Player/player.gd
+++ Src/Assets/Player/player.gd
@@ -1,9 +1,29 @@
 extends CharacterBody2D
 
 var speed = 400  # speed in pixels/sec
-
+var available_mask = [
+	"Miskin",
+	"Mengengah",
+	"Kaya",
+	"Konglo"
+	]
+var current_mask = available_mask[0]
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
-
+	_swapping_mask()
 	move_and_slide()
+
+func _swapping_mask():
+	if Input.is_action_just_pressed("first_mask"):
+		current_mask = available_mask[0]
+		print("swapped to: "+current_mask)
+	elif Input.is_action_just_pressed("2nd_mask"):
+		current_mask = available_mask[1]
+		print("swapped to: "+current_mask)
+	elif Input.is_action_just_pressed("3rd_mask"):
+		current_mask = available_mask[2]
+		print("swapped to: "+current_mask)
+	elif Input.is_action_just_pressed("4th_mask"):
+		current_mask = available_mask[3]
+		print("swapped to: "+current_mask)
