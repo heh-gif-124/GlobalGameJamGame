651f5dc0ffd0a1377b99d6ebe48a4bee756962c3
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-01-30 17:49:16 +0800

    more schtuff

--- Src/Assets/Areas/GetFood.gd
+++ Src/Assets/Areas/GetFood.gd
@@ -1,6 +1,6 @@
 extends Area2D
 class_name CounterArea
 
-func _food_obtain(body):
+func _food_obtain(body: Node2D) -> void:
 	Global.has_food = true
 	print("Food Obtained")
--- Src/Assets/Player/player.gd
+++ Src/Assets/Player/player.gd
@@ -7,23 +7,54 @@ var available_mask = [
 	"Kaya",
 	"Konglo"
 	]
+func _ready() -> void:
+	$Miskin.visible = true
+	$Miskin.play("default")
 var current_mask = available_mask[0]
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	
 	_swapping_mask()
+	_animation_handler()
 	move_and_slide()
 
 func _swapping_mask():
 	if Input.is_action_just_pressed("first_mask"):
 		current_mask = available_mask[0]
 		print("swapped to: "+current_mask)
+		$Miskin.visible = true
+		$Menengah.visible = false
 	elif Input.is_action_just_pressed("2nd_mask"):
 		current_mask = available_mask[1]
 		print("swapped to: "+current_mask)
+		$Miskin.visible = false
+		$Menengah.visible = true
 	elif Input.is_action_just_pressed("3rd_mask"):
 		current_mask = available_mask[2]
 		print("swapped to: "+current_mask)
 	elif Input.is_action_just_pressed("4th_mask"):
 		current_mask = available_mask[3]
 		print("swapped to: "+current_mask)
+
+func _animation_handler():
+	if current_mask == available_mask[0]:
+		if Global.has_food == true:
+			$Miskin.play("has_food")
+		else:
+			if Input.is_action_pressed("right"):
+				$Miskin.play("left_walk")
+			elif Input.is_action_pressed("left"):
+				$Miskin.play("right_walk_1")
+			else:
+				$Miskin.play("default")
+	elif  current_mask == available_mask[1]:
+		if Global.has_food == true:
+			$Menengah.play("has_food")
+		else:
+			if Input.is_action_pressed("right"):
+				$Menengah.play("left_walk")
+			elif Input.is_action_pressed("left"):
+				$Menengah.play("right_walk_1")
+			else:
+				$Menengah.play("default")
